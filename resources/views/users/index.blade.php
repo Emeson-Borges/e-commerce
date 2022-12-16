f9cf0586478ecda1643c4ad51fc95b5358bea550
Emeson Oliveira Borges <borges2016.leh@gmail.com>
2022-12-15 22:55:20 -0300

    Mostrando usuarios

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,1 +1,10 @@
-Listagem de Usuários
+<h1>Listagem de usuários</h1>
+
+<ul>
+@foreach ($users as $user)
+    <li>
+        {{ $user->name }} -
+        {{ $user->email }} 
+    </li>
+@endforeach
+</ul>
