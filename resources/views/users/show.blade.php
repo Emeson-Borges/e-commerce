5c0e4d556a202d53ad9d4831d588d802f6939c1a
Emeson Oliveira Borges <borges2016.leh@gmail.com>
2022-12-21 14:15:22 -0300

    Validação de email - search - segurança

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,6 +1,17 @@
+@extends('layouts.app')
+
+@section('title', 'Listagem2')
+
+@section('content')
 <h1>Listagem de usuário {{ $user->name }}</h1>
 
 <ul>
     <li>{{ $user->name }}</li>
     <li>{{ $user->email }}</li>
 </ul>
+<form action="{{ route('users.delete', $user->id) }}" method="POST">
+    @method('DELETE')
+    @csrf
+    <button type="submit">Deletar</button>
+</form>
+@endsection
